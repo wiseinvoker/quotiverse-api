e246c1bdbf1573c10c87541e69d17b58c8018266
wiseinvoker <grityangel@gmail.com>
2024-05-26 04:03:36 -0400

    feat: add middlewares

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).send('Access Denied');
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send('Invalid Token');
+    }
+};
